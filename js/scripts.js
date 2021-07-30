34f437465bfa8d13c35084398bf21595d80b07b8
nely de faria <nelly.nf54@gmail.com>
2021-07-30 14:04:50 -0300

    Adiciona arquivo scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+// Initial data
+let square = {
+    a1: '', a2: '', a3: '', 
+    b1: '', b2: '', b3: '', 
+    c1: '', c2: '', c3: ''
+};
+
+let player = '';
+let winner= '';
+let playing = false;
+
